58550fbb76fbc3a47a4c5117f408b279e0d5caa3
jsjtxietian <jsjtxietian@outlook.com>
2020-12-05 11:50:17 +0800

    open a windows window,vscode setup

--- code/build.bat
+++ code/build.bat
@@ -2,9 +2,7 @@
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
-mkdir ..\..\build
-pushd ..\..\build
-set PWD=%~dp0
-echo %PWD%
-cl -Zi ../HandmadeMonster/code/win32_handmade.cpp user32.lib
+mkdir ..\build
+pushd ..\build
+cl -Zi ../code/win32_handmade.cpp user32.lib gdi32.lib
 popd
