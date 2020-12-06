2f4d920631592d9e1ab5063a173c0cc41099df89
jsjtxietian <jsjtxietian@outlook.com>
2020-12-06 22:06:57 +0800

    day 004 , draw something with bitmap

--- code/build.bat
+++ code/build.bat
@@ -4,5 +4,5 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary
 
 mkdir ..\build
 pushd ..\build
-cl -Zi ../code/win32_handmade.cpp user32.lib gdi32.lib
+cl -FC -Zi ../code/win32_handmade.cpp user32.lib gdi32.lib
 popd
